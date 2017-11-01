e077bd0f34ae7138cf8418f5989a127ee76cb061
Roger Song <rogersong@Rogers-MacBook-Pro.local>
2017-11-02 10:59:45 +1300

    add content

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ else
     then 
         echo "Deploy to dev ec2 instances ..."
 	chmod 400 dev-test-ec2.pem
-	scp -i dev-test-ec2.pem -r buzz ec2-user@ec2-34-215-251-143.us-west-2.compute.amazonaws.com:/home/ec2-user/dataloader
-	scp -i dev-test-ec2.pem run.sh ec2-user@ec2-34-215-251-143.us-west-2.compute.amazonaws.com:/home/ec2-user/dataloader
+	scp -o stricthostkeychecking=no -i dev-test-ec2.pem -r buzz ec2-user@ec2-34-215-251-143.us-west-2.compute.amazonaws.com:/home/ec2-user/dataloader
+	scp -o stricthostkeychecking=no -i dev-test-ec2.pem run.sh ec2-user@ec2-34-215-251-143.us-west-2.compute.amazonaws.com:/home/ec2-user/dataloader
 	ssh -o stricthostkeychecking=no -i dev-test-ec2.pem ec2-user@ec2-34-215-251-143.us-west-2.compute.amazonaws.com "/home/ec2-user/dataloader/run.sh"
     fi
 fi
